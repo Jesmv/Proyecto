6df0b018f3cf4a2a9de272ad9a82b7783b8b1e76
Jessica Manso <jesmv83@gmail.com>
2017-05-28 14:15:32 +0200

    Cambios en MVC

--- view/homeView.php
+++ view/homeView.php
@@ -8,23 +8,101 @@
 
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+	<link rel="stylesheet" type="text/css" href="estilos.css">
 	
 </head>
 <body>
-	<h1><?php echo $title ?></h1>
-
-	<?php echo $helper->url('Home', 'viewHome') ?>
-
-	<p>
-		<?php var_dump($users) ?>
-	</p>
-
-	Fecha: <?php echo($date) ?>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active "><a class="link" href="#">Home</a></li>
+            <li><a class="link" href="#about">About</a></li>
+            <li><a class="link"  href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 	
 
+	<div class="container">
+		<h1>titulillo</h1>
+		<p>Lorem ipsum</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor in nisi a sollicitudin. 
+			Praesent tincidunt cursus ante sed ultrices. Integer sit amet fermentum velit. Integer consequat diam vitae felis suscipit ultricies. 
+			Ut ut commodo augue, fermentum dapibus arcu. Vestibulum pharetra consectetur porta. Donec tincidunt neque a velit dignissim efficitur. 
+			Mauris in porta augue, in elementum lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+			Vivamus in rutrum ex, eu cursus ligula. Suspendisse potenti. Phasellus volutpat tempor enim, ac feugiat ligula fermentum vitae. Nam 
+			rutrum vehicula feugiat. Maecenas pulvinar facilisis lectus nec dignissim. Mauris sollicitudin cursus nisl, vitae laoreet velit finibus vel.</p>
+
+		<p>Donec fringilla massa vitae euismod porttitor. In placerat efficitur luctus. Quisque tincidunt dolor non ante ullamcorper, ut imperdiet ipsum ultricies. 
+			Cras sed quam dolor. Nam et quam nec magna suscipit fringilla. Fusce suscipit velit neque, eget accumsan risus porttitor non. Phasellus fringilla ante diam. 
+			Donec mattis, leo sit amet tincidunt venenatis, ipsum velit dictum augue, tempus rhoncus neque mi sed sapien. Cras ullamcorper magna diam, posuere vehicula 
+			quam fermentum sed. Donec sapien turpis, sollicitudin ut lorem ut, accumsan condimentum turpis. Aenean iaculis eros justo, a tempor quam malesuada at. 
+			Maecenas convallis dui viverra purus eleifend luctus. Duis in mi id odio fermentum convallis a sed urna. Cras euismod diam vitae dignissim laoreet. 
+			Etiam sagittis, libero laoreet ultrices commodo, arcu sem elementum eros, sit amet dignissim ante libero nec est.</p>
+
+		<p>Donec mattis orci massa, vel consectetur tellus tristique ac. Curabitur ut lacus eu enim fringilla cursus eu quis enim. Donec eget viverra lectus. 
+			Vestibulum at pellentesque magna, a bibendum est. Vestibulum ac odio et mauris porta sodales a ut mi. Ut interdum nec ex sit amet interdum. 
+			Nam accumsan rhoncus eleifend. Nullam in libero vulputate, tincidunt diam et, malesuada justo. Pellentesque pretium, tortor ut aliquam dictum, mi ex 
+			vestibulum enim, sed tempor elit tortor non diam. Fusce tempor purus vitae turpis porta, eu viverra arcu sodales.</p>
+
+		<p>Sed interdum tincidunt neque, in accumsan metus dapibus ac. Nam ultrices mi ut quam luctus lobortis. Etiam sit amet ante ipsum. Donec consequat 
+			pharetra pellentesque. Mauris volutpat id lacus a vestibulum. Nulla facilisi. Etiam vitae iaculis massa. Phasellus imperdiet ac justo vel fermentum. 
+			Quisque varius turpis nec egestas dignissim. Phasellus ultrices id augue sed congue. Duis libero justo, sodales ac pharetra eu, fermentum at quam. 
+			Donec hendrerit magna in sodales finibus. Maecenas posuere quis leo nec ullamcorper.</p>
+
+		<p>Curabitur imperdiet finibus nunc, sit amet aliquet eros tincidunt vitae. Donec vitae urna ut nisi egestas lobortis id vel massa. Mauris lacinia tincidunt 
+		    egestas. Mauris vestibulum metus eu mi tempus, et aliquam eros hendrerit. Donec vestibulum efficitur lacus, id luctus urna pretium sed. Nullam dignissim, 
+			nisi eu pharetra ultrices, mi est tristique mauris, vel mollis lorem neque vitae est. Nunc ante massa, bibendum vitae viverra at, dictum quis risus. 
+			Maecenas sagittis arcu ante, id laoreet purus efficitur et. Vestibulum a laoreet urna, quis imperdiet ante. Cras tincidunt leo in quam molestie feugiat. S
+			uspendisse dignissim, lacus id maximus rutrum, velit erat vestibulum metus, vel imperdiet lorem velit at dui. Nunc sagittis luctus ipsum a mollis. 
+			Curabitur mattis viverra nisl, at bibendum eros mattis non.</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor in nisi a sollicitudin. 
+			Praesent tincidunt cursus ante sed ultrices. Integer sit amet fermentum velit. Integer consequat diam vitae felis suscipit ultricies. 
+			Ut ut commodo augue, fermentum dapibus arcu. Vestibulum pharetra consectetur porta. Donec tincidunt neque a velit dignissim efficitur. 
+			Mauris in porta augue, in elementum lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+			Vivamus in rutrum ex, eu cursus ligula. Suspendisse potenti. Phasellus volutpat tempor enim, ac feugiat ligula fermentum vitae. Nam 
+			rutrum vehicula feugiat. Maecenas pulvinar facilisis lectus nec dignissim. Mauris sollicitudin cursus nisl, vitae laoreet velit finibus vel.</p>
+
+		<p>Donec fringilla massa vitae euismod porttitor. In placerat efficitur luctus. Quisque tincidunt dolor non ante ullamcorper, ut imperdiet ipsum ultricies. 
+			Cras sed quam dolor. Nam et quam nec magna suscipit fringilla. Fusce suscipit velit neque, eget accumsan risus porttitor non. Phasellus fringilla ante diam. 
+			Donec mattis, leo sit amet tincidunt venenatis, ipsum velit dictum augue, tempus rhoncus neque mi sed sapien. Cras ullamcorper magna diam, posuere vehicula 
+			quam fermentum sed. Donec sapien turpis, sollicitudin ut lorem ut, accumsan condimentum turpis. Aenean iaculis eros justo, a tempor quam malesuada at. 
+			Maecenas convallis dui viverra purus eleifend luctus. Duis in mi id odio fermentum convallis a sed urna. Cras euismod diam vitae dignissim laoreet. 
+			Etiam sagittis, libero laoreet ultrices commodo, arcu sem elementum eros, sit amet dignissim ante libero nec est.</p>
+
+		<p>Donec mattis orci massa, vel consectetur tellus tristique ac. Curabitur ut lacus eu enim fringilla cursus eu quis enim. Donec eget viverra lectus. 
+			Vestibulum at pellentesque magna, a bibendum est. Vestibulum ac odio et mauris porta sodales a ut mi. Ut interdum nec ex sit amet interdum. 
+			Nam accumsan rhoncus eleifend. Nullam in libero vulputate, tincidunt diam et, malesuada justo. Pellentesque pretium, tortor ut aliquam dictum, mi ex 
+			vestibulum enim, sed tempor elit tortor non diam. Fusce tempor purus vitae turpis porta, eu viverra arcu sodales.</p>
+
+		<p>Sed interdum tincidunt neque, in accumsan metus dapibus ac. Nam ultrices mi ut quam luctus lobortis. Etiam sit amet ante ipsum. Donec consequat 
+			pharetra pellentesque. Mauris volutpat id lacus a vestibulum. Nulla facilisi. Etiam vitae iaculis massa. Phasellus imperdiet ac justo vel fermentum. 
+			Quisque varius turpis nec egestas dignissim. Phasellus ultrices id augue sed congue. Duis libero justo, sodales ac pharetra eu, fermentum at quam. 
+			Donec hendrerit magna in sodales finibus. Maecenas posuere quis leo nec ullamcorper.</p>
+
+		<p>Curabitur imperdiet finibus nunc, sit amet aliquet eros tincidunt vitae. Donec vitae urna ut nisi egestas lobortis id vel massa. Mauris lacinia tincidunt 
+		    egestas. Mauris vestibulum metus eu mi tempus, et aliquam eros hendrerit. Donec vestibulum efficitur lacus, id luctus urna pretium sed. Nullam dignissim, 
+			nisi eu pharetra ultrices, mi est tristique mauris, vel mollis lorem neque vitae est. Nunc ante massa, bibendum vitae viverra at, dictum quis risus. 
+			Maecenas sagittis arcu ante, id laoreet purus efficitur et. Vestibulum a laoreet urna, quis imperdiet ante. Cras tincidunt leo in quam molestie feugiat. S
+			uspendisse dignissim, lacus id maximus rutrum, velit erat vestibulum metus, vel imperdiet lorem velit at dui. Nunc sagittis luctus ipsum a mollis. 
+			Curabitur mattis viverra nisl, at bibendum eros mattis non.</p>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
